7db1dc501c910869c23193b70e7b55d8d313198d
Arisnew <aris.new2011@gmail.com>
2020-07-01 11:51:57 +0700

    relasi attendee & tampilan session

--- ars_academy/models/session.py
+++ ars_academy/models/session.py
@@ -31,6 +31,37 @@ class Session(models.Model):
     description = fields.Text(
         string='Description',
     )
+
+    attendee_ids = fields.One2many(
+        comodel_name='ars.academy.attendee', 
+        inverse_name='session_id', 
+        string='Attendees',
+    )
+        
     
+class Attendee(models.Model):
+    _name = 'ars.academy.attendee'
+    _description = 'Attendees of session'
+
+    name = fields.Char(
+        string = 'Reg No.',
+        required = True,
+    )
+
+    reg_date = fields.Datetime(
+        string='Reg Date',
+        required = True,
+        default = fields.Datetime.now,
+    )
     
-    
+    session_id = fields.Many2one(
+        comodel_name='ars.academy.session', 
+        string='Session',
+    )
+
+    student_id = fields.Many2one(
+        comodel_name='res.partner', 
+        string='Student',
+        domain = [('is_student', '=', True)],
+        required = True,
+    )
